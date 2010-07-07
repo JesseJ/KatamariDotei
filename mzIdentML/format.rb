a2d2d4fa4ea3fbda102536d2923358101c87cde3
John Prince <jtprince@gmail.com>
2010-07-07 17:18:44 -0600

    review by jtprince

--- mzIdentML/format.rb
+++ mzIdentML/format.rb
@@ -1,13 +1,15 @@
 require "#{File.dirname($0)}/spect_id_result.rb"
 require 'nokogiri'
 
+require 'pp'
+
 #A base class for other file formats. Other formats are meant to inherit from this class, thus Format is basically useless by itself.
 #Contains some methods that are applicable to all formats.
 #Classes that inherit from Format are used as the means of obtaining information from a file to be used in Search2mzIdentML.
 #Takes a string containing the search output file location and a string containing the FASTA  database that was used.
 class Format
   def initialize(file, database)
-    puts "\nPreparing..."
+    puts "\nPreparing..." if $VERBOSE
     
     @file = file
     @database = database
@@ -38,65 +40,67 @@ class Format
     ""
   end
   
-  #Retrieves the threshold value
+  # Retrieves the threshold value
   def threshold
     0
   end
 	
-  #Retrieves all the proteins
+  # Retrieves all the proteins
   def proteins
     []
   end
   
-  #Retrieves all the peptides
+  # Retrieves all the peptides
   def peptides
     []
   end
   
-  #Retrieves the name of the search database that was used
+  # Retrieves the name of the search database that was used
   def databaseName
     ""
   end
   
-  #Retrieves the results of the search engine
+  # Retrieves the results of the search engine
   def results
     []
   end
   
-  #Retrieves the number of database sequences
+  # Retrieves the number of database sequences
   def numberOfSequences
     0
   end
   
-  #Converts calc_neutral_pep_mass to calculatedMassToCharge
+  # Converts calc_neutral_pep_mass to calculatedMassToCharge
   def calMass(mass, charge)
+    
     (mass + (charge.to_f * 1.00727646677)) / charge
   end
   			
-  #Converts calc_neutral_pep_mass to experimentalMassToCharge
+  # Converts calc_neutral_pep_mass to experimentalMassToCharge
   def experiMass(mass, charge, diff)
     ((mass + diff) + (charge.to_f * 1.00727646677)) / charge
   end
   
-  #Determines the accession number for the score type. For some reason, this has become the slowest part of the conversion.
-  #Might be able to increase speed by switching to hash, or removing uneeded terms.
+  # Determines the accession number for the score type. For some reason, this has become the slowest part of the conversion.
   def findAccession(name)
-    arr = @obo[name]
-    
-    return arr[0], arr[1] if arr != nil
-    return "", ""
+    if arr = @obo[name]
+      arr
+    else
+      ["", ""]
+    end
   end
 	
-  #Conforms score name to mzIdentML format
+  # Conforms score name to mzIdentML format
   def conformScoreName(name, engine)
-    if engine == "X! Tandem"
-      name = "xtandem:" + name
-    elsif engine == "MASCOT"
-      name = "mascot:" + name
-    elsif engine == "OMSSA"
-      name = "OMSSA:" + name
-    end
-    
-    name
+    base = 
+      case engine
+      when "X! Tandem"
+        "xtandem"
+      when "MASCOT"
+        "mascot"
+      when "OMSSA"
+        "OMSSA"
+      end
+    [base, name].join(':')
   end
 end
--- mzIdentML/spec.rb
+++ mzIdentML/spec.rb
@@ -8,16 +8,36 @@ describe 'PepXML2mzIdentML' do
     #@p2mo = Search2mzIdentML.new(PepXML.new("#{File.dirname($0)}/test-omssa.pep.xml", "#{File.dirname($0)}/../databases/uni_human_var_100517_fwd.fasta"))
     #@p2mt = Search2mzIdentML.new(PepXML.new("#{File.dirname($0)}/test-tandem.pep.xml", "#{File.dirname($0)}/../databases/uni_human_var_100517_fwd.fasta"))
   end
+
+  def same_header(doc1, doc2)
+  end
   
-  it 'takes a pepXML file and outputs an mzIdentML file' do
+  xit 'takes a pepXML file and outputs an mzIdentML file' do
     @p2mm.convert
     #@p2mo.convert
     #@p2mt.convert
         
     file1 = File.open("#{File.dirname($0)}/test-test.mzid")
     file2 = File.open("#{File.dirname($0)}/test.mzid")
-    FileUtils::cmp(file1, file2).is true
+
+
+    ok File.exist?(file1)
+    #FileUtils::cmp(file1, file2).is true
   end
 end
+
+
+describe 'dealing with protein IDs' do
+  it 'massages multiple ID types into a single, short ID' do
+    {'QSASDF123243' => 'QSASDF123243', 'ASDF12|crazy protein description' => 'ASDF12', '1245' => 'hellonewprotein', 'IPI:IPI123451|QGASDF|ASDASDF' => 'IPI123451'}.each do |test, expected|
+      obj = Object.new
+      obj.instance_variable_set("@proteinIndices", {1245 => 'hellonewprotein'})
+      obj.extend(PepXML::ProteinID)
+      obj.proteinID(test).is expected
+    end
+  end
+end
+
+
 
 Bacon.summary_on_exit
